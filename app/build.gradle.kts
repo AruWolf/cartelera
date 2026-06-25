bee4ca85413328d824fd91d6e5643f4b0ed09057
Leinho314 <morales.leonardo.mo@gmail.com>
2026-06-25 10:10:39 -0300

    New version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.litvy.carteleria"
         minSdk = 21
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     buildFeatures {
