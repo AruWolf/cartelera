4079fde5ced65ce1f26f278b7aea3e372cb22187
Leinho314 <morales.leonardo.mo@gmail.com>
2026-07-15 10:41:57 -0300

    2.0 Version instance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.litvy.carteleria"
         minSdk = 23
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.9.4"
+        versionCode = 14
+        versionName = "2.0"
     }
 
     buildFeatures {
